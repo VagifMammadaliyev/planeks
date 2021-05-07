2e03dac6259f54ca56e95721ad3f5ee88c9bfdaf
VagifMammadaliyev <vagifmammadaliyev@outlook.com>
2021-05-08 03:44:35 +0400

    dataset model created & admin interface

--- app/dataschemas/admin.py
+++ app/dataschemas/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import DataColumn, DataSchema
+from .models import DataColumn, DataSchema, DataSet
 
 
 class DataColumnInlineAdmin(admin.TabularInline):
@@ -16,3 +16,25 @@ class DataSchemaAdmin(admin.ModelAdmin):
     readonly_fields = ["created_at", "updated_at"]
     list_select_related = ["user"]
     inlines = [DataColumnInlineAdmin]
+
+
+@admin.register(DataSet)
+class DataSetAdmin(admin.ModelAdmin):
+    autocomplete_fields = ["schema"]
+    search_fields = ["schema__title", "schema__user__email"]
+    list_display = [
+        "schema",
+        "started_generation_at",
+        "finished_generation_at",
+        "generated_file",
+        "is_finished",
+    ]
+    readonly_fields = [
+        "created_at",
+        "finished_generation_at",
+        "started_generation_at",
+        "is_started",
+        "is_finished",
+        "is_processing",
+    ]
+    list_select_related = ["schema"]
