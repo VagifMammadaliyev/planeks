91094e2afd2987b9466b457cfbe6af0580a630de
VagifMammadaliyev <vagifmammadaliyev@outlook.com>
2021-05-08 13:45:13 +0400

    tasks must not rely on models, but models can rely on tasks

--- app/dataschemas/tasks.py
+++ app/dataschemas/tasks.py
@@ -1,11 +1,12 @@
 from celery import shared_task
 
 from planeks.celery import QUEUES
-from dataschemas.models import DataSet
 
 
 @shared_task(queue=QUEUES.GENERATOR_QUEUE)
 def generate_csv(dataset_id):
+    from .models import DataSet
+
     dataset = DataSet.objects.filter(id=dataset_id).first()
     if not dataset:
         return f"Dataset with id {dataset_id} not found"
